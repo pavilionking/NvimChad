2fb5e48583fe419783ec97607ff53f3e5c9cbdd0
peii <yifeiting@gmail.com>
2023-09-12 18:38:44 +0800

    fix: todo height

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -64,6 +64,7 @@ local plugins = {
   {
     'folke/todo-comments.nvim',
     dependencies = 'nvim-lua/plenary.nvim',
+    lazy = false,
     config = function() 
       require('todo-comments').setup()
     end,
